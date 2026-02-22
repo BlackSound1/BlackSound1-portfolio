a88a5b678cbf6dddd598ea31e1139cb7428b2fa4
BlackSound1 <matthewr@videotron.ca>
2026-02-21 19:34:17 -0500

    Added home link to nav header

--- components/ui/NavHeader.tsx
+++ components/ui/NavHeader.tsx
@@ -3,7 +3,11 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <div className="header sticky top-0 z-10 flex items-center justify-between p-5 select-none mb-[-5]">
-      <div></div>
+      <nav className='font-mono'>
+        <Link className="navitem transition-colors duration-200" href="/">
+          ~/_
+        </Link>
+      </nav>
       <nav className="font-mono">
         <Link className="navitem px-3 transition-colors duration-200" href="/research">
           Research

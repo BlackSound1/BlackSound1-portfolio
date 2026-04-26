9aee0a1d281e6c345e69a60b6a099eda2090685d
BlackSound1 <matthewr@videotron.ca>
2026-04-25 22:15:03 -0400

    Test Header

---
+++ __tests__/components/ui/navHeader.test.tsx
@@ -0,0 +1,24 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Header from "@/components/ui/NavHeader";
+
+describe("NavHeader", () => {
+    it("renders the NavHeader", async () => {
+        render(<Header />);
+
+        const allLinks = await screen.findAllByRole("link");
+        
+        expect(allLinks.length).toBe(4);
+        
+        const home = screen.getByText("~/_");
+        const research = screen.getByText("Research");
+        const uses = screen.getByText("Uses");
+        const links = screen.getByText("Links");
+
+        expect(home).toBeInTheDocument();
+        expect(research).toBeInTheDocument();
+        expect(uses).toBeInTheDocument();
+        expect(links).toBeInTheDocument();
+    });
+});

225434d70342b8ff0d9f759aa68855737ad75576
BlackSound1 <matthewr@videotron.ca>
2026-04-26 13:31:39 -0400

    Test Tag and SoundcloudEmbed. Improve Jest config

---
+++ __tests__/components/ui/tag.test.tsx
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Tag from "@/components/ui/tag";
+import { ThemeProvider } from "@/context/ThemeContext";
+
+describe("Tag", () => {
+  it("renders a tag", async () => {
+    render(
+      <ThemeProvider>
+        <Tag lang="test" key="test" />
+      </ThemeProvider>,
+    );
+
+    const tag = await screen.findByTestId("tag");
+
+    expect(tag).toBeInTheDocument();
+
+    expect(tag).toHaveClass("tag");
+
+    expect(tag).toHaveTextContent("test");
+  });
+});
